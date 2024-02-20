f620f12178238905532b62d558061955721dfc85
Trey <trey.shearin3@gmail.com>
2024-02-20 15:30:53 -0500

    added functionality for remaining time row buttons, expanded save all functionality to encompass new rows

--- script.js
+++ script.js
@@ -26,6 +26,11 @@ $(function () {
     let text10Input = $("#text10");
     let text11Input = $("#text11");
     let text12Input = $("#text12");
+    let text1Input = $("#text1");
+    let text2Input = $("#text2");
+    let text3Input = $("#text3");
+    let text4Input = $("#text4");
+    let text5Input = $("#text5");
     
 // Setting text area values on page load from local storage values
     $(document).ready(function() {
@@ -33,9 +38,15 @@ $(function () {
         text10Input.val(localStorage.getItem("tenOClock"));
         text11Input.val(localStorage.getItem("elevenOClock"));
         text12Input.val(localStorage.getItem("twelveOClock"));
+        text1Input.val(localStorage.getItem("oneOClock"));
+        text2Input.val(localStorage.getItem("twoOClock"));
+        text3Input.val(localStorage.getItem("threeOClock"));
+        text4Input.val(localStorage.getItem("fourOClock"));
+        text5Input.val(localStorage.getItem("fiveOClock"));
+
     })
 
-// Declaring function to save user inputs with separate local storage keys
+// Declaring functions to save user inputs with separate local storage keys
     function saveInput9() {
         localStorage.setItem("nineOClock", text9Input.val());
     }
@@ -48,18 +59,45 @@ $(function () {
     function saveInput12() {
         localStorage.setItem("twelveOClock", text12Input.val());
     }
+    function saveInput1() {
+        localStorage.setItem("oneOClock", text1Input.val());
+    }
+    function saveInput2() {
+        localStorage.setItem("twoOClock", text2Input.val());
+    }
+    function saveInput3() {
+        localStorage.setItem("threeOClock", text3Input.val());
+    }
+    function saveInput4() {
+        localStorage.setItem("fourOClock", text4Input.val());
+    }
+    function saveInput5() {
+        localStorage.setItem("fiveOClock", text5Input.val());
+    }
+//Function to save all text area input data at once    
     function saveAllInputs(){
         localStorage.setItem("nineOClock", text9Input.val());
         localStorage.setItem("tenOClock", text10Input.val());
         localStorage.setItem("elevenOClock", text11Input.val());
         localStorage.setItem("twelveOClock", text12Input.val());
+        localStorage.setItem("oneOClock", text1Input.val());
+        localStorage.setItem("twoOClock", text2Input.val());
+        localStorage.setItem("threeOClock", text3Input.val());
+        localStorage.setItem("fourOClock", text4Input.val());
+        localStorage.setItem("fiveOClock", text5Input.val());
     }
-// Adding event listeners for hourly buttons to save user inputs
+// Event listeners for hourly buttons to save user inputs
     button9.addEventListener("click", saveInput9);
     button10.addEventListener("click", saveInput10);
     button11.addEventListener("click", saveInput11);
     button12.addEventListener("click", saveInput12);
+    button1.addEventListener("click", saveInput1);
+    button2.addEventListener("click", saveInput2);
+    button3.addEventListener("click", saveInput3);
+    button4.addEventListener("click", saveInput4);
+    button5.addEventListener("click", saveInput5);
 
+// Event listener for save all button    
     saveAllBtn.addEventListener("click", saveAllInputs);
 
 
